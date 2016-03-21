3eb952b10034575b8c440bbfff4a0fb094ac1b61
izabera <izaberina@gmail.com>
2016-03-21 04:25:11 +0100

    gcc pragmas

--- lib/flags.h
+++ lib/flags.h
@@ -23,12 +23,15 @@ struct opts {
 
 int parseopts(int argc, char *argv[], const char *program, struct opts opts);
 
-#define options(...) switch (parseopts(argc-1, argv+1, __func__,    \
-                                  (struct opts) { __VA_ARGS__ })) { \
-                       case 'h': return 0;                          \
-                       case '?': case ':': case '@': return 1;      \
-                     }                                              \
-                     argc -= parseoptind;                           \
+#define options(...) _Pragma("GCC diagnostic push")                                     \
+                     _Pragma("GCC diagnostic ignored \"-Wmissing-field-initializers\"") \
+                     switch (parseopts(argc-1, argv+1, __func__,                        \
+                                  (struct opts) { __VA_ARGS__ })) {                     \
+                     _Pragma("GCC diagnostic pop")                                      \
+                       case 'h': return 0;                                              \
+                       case '?': case ':': case '@': return 1;                          \
+                     }                                                                  \
+                     argc -= parseoptind;                                               \
                      argv += parseoptind;
 
 #define flag(x) flags[opt(x)].count
