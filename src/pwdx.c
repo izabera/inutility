f669bd876ba82b0e0da5b080bc3244da7b1f1850
izabera <izaberina@gmail.com>
2016-03-27 13:55:23 +0200

    better pwdx

--- src/pwdx.c
+++ src/pwdx.c
@@ -2,12 +2,14 @@
 
 int main(int argc, char *argv[]) {
   options("", .argleast = 1);
-  char *proc, dir[PATH_MAX];
+  char proc[20], dir[PATH_MAX];
+  ssize_t lnk;
   while (*++argv) {
-    asprintf(&proc, "/proc/%s/cwd", *argv);
-    if (readlink(proc, dir, PATH_MAX-1) != -1)
+    snprintf(proc, sizeof(proc), "/proc/%ld/cwd", strtol(*argv, NULL, 10));
+    if ((lnk = readlink(proc, dir, PATH_MAX-1)) != -1) {
+      dir[lnk] = 0;
       printf("%s: %s\n", *argv, dir);
-    free(proc);
+    }
   }
   return errno;
 }
