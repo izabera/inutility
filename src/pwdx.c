22663b0f861ea4fc23971ad63fa6f1bfd7b3f15b
izabera <izaberina@gmail.com>
2016-03-27 14:06:36 +0200

    pwdx /proc/pid

--- src/pwdx.c
+++ src/pwdx.c
@@ -5,7 +5,8 @@ int main(int argc, char *argv[]) {
   char proc[20], dir[PATH_MAX];
   ssize_t lnk;
   while (*++argv) {
-    snprintf(proc, sizeof(proc), "/proc/%ld/cwd", strtol(*argv, NULL, 10));
+    snprintf(proc, sizeof(proc), "/proc/%ld/cwd",
+        strtol(*argv + (6 * !strncmp("/proc/", *argv, 6)), NULL, 10)); // useless
     if ((lnk = readlink(proc, dir, PATH_MAX-1)) != -1) {
       dir[lnk] = 0;
       printf("%s: %s\n", *argv, dir);
