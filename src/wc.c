e8a515c4cd6b91a6d93252aacdb1a6ae18b1d4dc
izabera <izaberina@gmail.com>
2016-07-08 08:18:08 +0200

    wc

---
+++ src/wc.c
@@ -0,0 +1,80 @@
+#include "lib/common.h"
+
+struct Wc { size_t w, c, m, l; };
+#define bufsize 4096
+
+static void printwc(struct Wc count, const char *name) {
+  if (flag('l')) printf("%7zu ", count.l);
+  if (flag('w')) printf("%7zu ", count.w);
+  if (flag('c')) printf("%7zu ", count.c);
+  if (flag('m')) printf("%7zu ", count.m);
+  if (name) printf("%s", name);
+  putchar('\n');
+}
+
+struct Wc wc(int fd) { /* counts both utf8 chars and bytes, assuming valid utf8 */
+  size_t words = 0, chars = 0, bytes = 0, lines = 0;
+  int inword = 0;
+  unsigned char buffer[bufsize];
+  ssize_t len;
+  while ((len = read(fd, &buffer, bufsize)) > 0) {
+    for (int i = 0; i < len; i++) {
+      switch (buffer[i]) {
+        case '\n': lines++;
+        case ' ': case '\t': case '\r': case '\f': case '\v':
+          if (inword) {
+            inword = 0;
+            words++;
+          }
+          break;
+        default: inword = 1;
+      }
+      chars += (buffer[i] < 0x80 || buffer[i] > 0xBF);
+    }
+    bytes += len;
+  }
+  return (struct Wc){ words, chars, bytes, lines };
+}
+
+int main(int argc, char *argv[]) {
+  options("wclm");
+  int fl = (!!flag('w')) | (!!flag('c'))<<1 || flag('l')<<2 | (!!flag('m'))<<3;
+  int saverrno, file = 0;
+  FILE *fileptr = stdin;
+  if (!fl) {
+    fl = 1|2|4;
+    flag('c') = 1;
+    flag('l') = 1;
+    flag('w') = 1;
+  }
+  struct Wc count = { 0 }, tot = { 0 };
+
+  if (argc == 1)
+    goto inner;
+  while (*++argv) {
+         if (argv[0][0] == '-' && argv[0][1] == 0) { file = 0; fileptr = stdin; }
+    else if ((file = open(argv[0], O_RDONLY)) == -1) continue;
+    else fileptr = fdopen(file, "r");
+inner:
+    saverrno = errno;
+    if (fl == 2) {
+      struct stat st;
+      fstat(file, &st);
+      if (!S_ISREG(st.st_mode) || st.st_size == 0) goto readit;
+      count.c = 0;
+    }
+    else {
+readit:
+      errno = saverrno;
+      count = wc(file);
+      tot.c += count.c;
+      tot.l += count.l;
+      tot.m += count.m;
+      tot.w += count.w;
+      printwc(count, argc == 1 ? NULL : *argv);
+    }
+    if (fileptr != stdin) close(file);
+  }
+  if (argc > 2) printwc(tot, "total");
+  return errno;
+}
