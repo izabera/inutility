5ed8cacd3a88383dcac527752bf636fa70f4d1f8
izabera <izaberina@gmail.com>
2016-07-09 08:26:56 +0200

    truncate

---
+++ skel.c
@@ -0,0 +1,16 @@
+#include "lib/common.h"
+
+int main(int argc, char *argv[]) {
+  options("");
+  FILE *fileptr = stdin;
+  if (argc == 1) {
+    argv[0] = "-";
+    goto inner;
+  }
+  while (*++argv) {
+         if (argv[0][0] == '-' && argv[0][1] == 0) fileptr = stdin;
+    else if (!(fileptr = fopen(argv[0], "r"))) continue;
+inner:
+  }
+  return errno;
+}
---
+++ src/truncate.c
@@ -0,0 +1,22 @@
+#include "lib/common.h"
+
+int main(int argc, char *argv[]) {
+  options("cr:s*", .argleast = 1);
+  int file;
+  size_t siz;
+  struct stat st;
+  if (flag('s')) siz = lastnum('s');
+  else if (flag('r')) {
+    if (stat(lastarg('r'), &st) == -1) return errno;
+    siz = st.st_size;
+  }
+  else return 1;
+
+  while (*++argv) {
+         if (argv[0][0] == '-' && argv[0][1] == 0) file = 0;
+    else if ((file = open(argv[0], O_WRONLY|((!flag('c'))*O_CREAT), 0666)) == -1) continue;
+    ftruncate(file, siz);
+    if (file) close(file);
+  }
+  return errno;
+}
