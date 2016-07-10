3daadc811ef5fc3a8c8d1811f1558867019443f6
izabera <izaberina@gmail.com>
2016-07-10 15:32:28 +0200

    fix pwd

--- src/pwd.c
+++ src/pwd.c
@@ -2,15 +2,20 @@
 
 int main(int argc, char *argv[]) {
   options("LP", .arglessthan = 1);
-  char *output = NULL;
+  char *output = NULL, *env = getenv("PWD"), *ptr = env;
   struct stat st1, st2;
   if (!flag('P')) {
-    char *env = getenv("PWD");
-    if (env && !stat(env, &st1) && !stat(".", &st2)
-        && (st1.st_dev == st2.st_dev) && (st1.st_ino == st2.st_ino))
-      output = env;
+    if (env && *env == '/') {
+      do {
+        if (*ptr++ == '.' && (*ptr == '/' || *ptr == 0)) goto nope;
+        if (*ptr == '.' && (ptr[1] == '/' || ptr[1] == 0)) goto nope;
+      } while (*ptr);
+      if (!stat(env, &st1) && !stat(".", &st2)
+          && st1.st_dev == st2.st_dev && st1.st_ino == st2.st_ino)
+        output = env;
+    }
   }
-  errno = 0;
+nope: errno = 0;
   if (output) puts(output);
   else puts(getcwd(NULL, 0)); // glibc and musl and more
   return errno;
