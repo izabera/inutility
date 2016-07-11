b41c5c74c80fa9062febb9037ea76c3f6aa1241e
izabera <izaberina@gmail.com>
2016-07-11 09:10:16 +0200

    uniq -D

--- src/uniq.c
+++ src/uniq.c
@@ -1,15 +1,15 @@
 #include "lib/common.h"
 
 #define printline() do {                                                            \
-                      if ((flag('d') && count != 1) ||                              \
+                      if (((flag('d')|flag('D')) && count != 1) ||                  \
                           (flag('u') && count == 1) ||                              \
-                          (!flag('d') && !flag('u'))) {                             \
+                          (!(flag('d')|flag('D')) && !flag('u'))) {                 \
                         if (flag('c')) printf("%7zu %s%c", count, prevline, delim); \
                         else printf("%s%c", prevline, delim);                       \
                       }                                                             \
                     } while (0)
 int main(int argc, char *argv[]) {
-  options("cdiuzs|w|", .arglessthan = 3);
+  options("cdDiuzs|w|", .arglessthan = 3);
   if (argc >= 2) if (!(freopen(argv[1], "r", stdin))) return errno;
   if (argc == 3) if (!(freopen(argv[2], "w", stdout))) return errno;
   if (flag('s')) flag('s') = lastnum('s');
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
     else if (flag('w') && flag('w') > flag('s'))
       same = !comparen(line+flag('s'), prevline+flag('s'), flag('w')-flag('s'));
     else same = !compare(line+flag('s'), prevline+flag('s'));
-    if (same) count++;
+    if (same) { count++; if (flag('D')) printline(); }
     else { printline(); count = 1; }
     free(prevline);
     prevline = strdup(line);
