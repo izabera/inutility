91e040028c7163685090aa6bd1806d13ed4386ea
izabera <izaberina@gmail.com>
2016-09-25 19:01:31 +0200

    ybden reported a bug in copyfd

--- lib/copyfd.c
+++ lib/copyfd.c
@@ -5,9 +5,10 @@ void copyfd(int in, int out) {
   ssize_t size;
   while ((size = sendfile(out, in, NULL, 1 << 24)) > 0) { } // try sendfile first and fall back
   errno = saverrno;
-  if (size) {// both positive or -1
+  if (size) {// -1
     char buf[IBUFSIZ];
     while ((size = read(in, buf, sizeof(buf))) > 0)
-      for (ssize_t w = 0; w != -1 && (size -= w); w = write(out, buf, size)) ; // maybe use stdio?
+      for (ssize_t w = 0, off = 0; w != -1 && (size -= w); w = write(out, buf+off, size))
+        off += w; // maybe use stdio?
   }
 }
