68d107d27862a4f915cbda99811d4a8acc16345f
izabera <izaberina@gmail.com>
2016-12-06 11:07:27 +0100

    echo because why not

---
+++ src/echo.c
@@ -0,0 +1,10 @@
+#include "lib/common.h"
+
+int main(int argc, char *argv[]) {
+  options("n");
+  if (argc > 1)
+    for (int i = 1; i < argc; i++)
+      printf("%s%s", i > 1 ? " " : "", argv[i]);
+  if (!flag('n')) putchar_unlocked('\n');
+  return errno;
+}
