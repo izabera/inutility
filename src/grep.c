e946ed86fad50d8ed7ad85a8b41497515531e29d
izabera <izaberina@gmail.com>
2016-12-08 10:11:46 +0100

    grep -x

--- src/grep.c
+++ src/grep.c
@@ -1,29 +1,34 @@
 #include "lib/common.h"
 
 static regex_t *regs;
-static char **patterns = NULL;
-static int regmatch(const char *str, size_t i) {
-  return regexec(regs+i, str, 0, NULL, 0) == 0 ? 1 : -1;
+static struct str *patterns = NULL;
+static int regmatch(struct str s, size_t i) {
+  regmatch_t m;
+  if (regexec(regs+i, s.str, 1, &m, 0) != 0) return -1;
+  if (flag('x') && (m.rm_so != 0 || (size_t)m.rm_eo != s.len)) return -1;
+  return 1;
 }
-static int strmatch(const char *str, size_t i) {
-  return strstr(str, patterns[i]) != 0 ? 1 : -1;
+static int strmatch(struct str s, size_t i) {
+  if (flag('x') && s.len != patterns[i].len) return -1;
+  return strstr(s.str, patterns[i].str) != 0 ? 1 : -1;
 }
-static int strcasematch(const char *str, size_t i) {
-  return strcasestr(str, patterns[i]) != 0 ? 1 : -1;
+static int strcasematch(struct str s, size_t i) {
+  if (flag('x') && s.len != patterns[i].len) return -1;
+  return strcasestr(s.str, patterns[i].str) != 0 ? 1 : -1;
 }
 
 int main(int argc, char *argv[]) {
-  options("acEFhHinqsve:f:"); // -as are ignored
+  options("acEFhHinqsvxe:f:"); // -as are ignored
   FILE *fileptr = stdin;
   ssize_t read;
-  size_t len, count, lineno;
-  char *line;
+  size_t count, lineno;
 
   size_t npatterns = 0;
   // strikingly efficient, great engineering
-#define push(p) do {                                            \
-    patterns = realloc(patterns, ++npatterns * sizeof(char*));  \
-    patterns[npatterns-1] = p;                                  \
+#define push(p, l) do {                                               \
+    patterns = realloc(patterns, ++npatterns * sizeof(struct str));   \
+    patterns[npatterns-1].str = p;                                    \
+    patterns[npatterns-1].len = l;                                    \
   } while (0)
 
   if (flag('e') || flag('f')) {
@@ -34,19 +39,20 @@ int main(int argc, char *argv[]) {
         do {
           char *tmp = NULL;
           size_t s = 0;
-          if (getline(&tmp, &s, ftmp) != -1) {
-            if (tmp[s-1] == '\n') tmp[s-1] = 0;
-            push(tmp);
+          if ((read = getline(&tmp, &s, ftmp)) != -1) {
+            if (tmp[read-1] == '\n') tmp[--read] = 0;
+            push(tmp, read);
           }
           else break;
         } while (1);
       }
     }
-    for (size_t i = 0; i < flag('e'); i++) push(flags[opt('c')].args[i]);
+    for (size_t i = 0; i < flag('e'); i++)
+      push(flags[opt('c')].args[i], strlen(flags[opt('c')].args[i]));
   }
   else {
     if (argc == 1) return 255;
-    push(argv[1]);
+    push(argv[1], strlen(argv[1]));
     argc--;
     argv++;
   }
@@ -54,12 +60,12 @@ int main(int argc, char *argv[]) {
   if (!flag('F')) {
     regs = malloc(sizeof(regex_t) * npatterns);
     for (size_t i = 0; i < npatterns; i++)
-      if (regcomp(regs+i, patterns[i],
-            REG_NOSUB | (!!flag('i')*REG_ICASE) | (!!flag('E')*REG_EXTENDED)) != 0)
+      if (regcomp(regs+i, patterns[i].str,
+            (!!flag('i')*REG_ICASE) | (!!flag('E')*REG_EXTENDED)) != 0)
           return 255;
   }
 
-  int (*matchfunc)(const char *, size_t) =
+  int (*matchfunc)(struct str, size_t) =
     flag('F') ? flag('i') ? strcasematch : strmatch : regmatch;
 
   int matched = 0, flagv = flag('v') ? -1 : 1;
@@ -73,7 +79,11 @@ int main(int argc, char *argv[]) {
     else if (!(fileptr = fopen(argv[0], "r"))) continue;
 inner:
     count = lineno = 0;
-    while ((read = getline(&line, &len, fileptr)) > 0) {
+    struct str line;
+    size_t tmp;
+    while ((read = getline(&line.str, &tmp, fileptr)) > 0) {
+      if (line.str[read-1] == '\n') line.str[--read] = 0;
+      line.len = read;
       lineno++;
       for (size_t i = 0; i < npatterns; i++)
         if (matchfunc(line, i) * flagv > 0) {
@@ -83,7 +93,7 @@ inner:
           else {
             if (flag('H') && !flag('h')) printf("%s:", *argv);
             if (flag('n')) printf("%zu:", lineno);
-            fputs_unlocked(line, stdout);
+            puts(line.str);
           }
         }
     }
