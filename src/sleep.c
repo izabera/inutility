bc762f228fccdb8adfa85d60e1d46f7826c6ee9e
izabera <izaberina@gmail.com>
2016-12-09 10:07:14 +0100

    add support for sleep 1h 20m

--- src/sleep.c
+++ src/sleep.c
@@ -1,20 +1,21 @@
 #include "lib/common.h"
 
 int main(int argc, char *argv[]) {
-  options("", .argleast = 1, .arglessthan = 2, .help = "number[suffix]");
-  argv++;
-  double arg = strtod(*argv, argv);
-  if (arg < 0) return -1;
-  switch (**argv) {
-    case 's': case 0:       break;
-    case 'm': arg *=    60; break;
-    case 'h': arg *=  3600; break;
-    case 'd': arg *= 86400; break;
-    default: return -1;
+  options("", .argleast = 1, .help = "number[suffix]...");
+  while (*++argv) { // sleep 2h 30m
+    double arg = strtod(*argv, argv);
+    if (arg < 0) return -1;
+    switch (**argv) {
+      case 's': case 0:       break;
+      case 'm': arg *=    60; break;
+      case 'h': arg *=  3600; break;
+      case 'd': arg *= 86400; break;
+      default: return -1;
+    }
+    struct timespec tv = { arg, 0 };
+    tv.tv_nsec = (arg - tv.tv_sec) * 1000000000;
+    nanosleep(&tv, NULL);
   }
-  struct timespec tv = { arg, 0 };
-  tv.tv_nsec = (arg - tv.tv_sec) * 1000000000;
-  nanosleep(&tv, NULL);
 
   return errno;
 }
