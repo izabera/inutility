c884d660e8e374ee85fb7853cadfcaf26c4873dc
izabera <izaberina@gmail.com>
2017-05-28 11:25:05 +0200

    fix date
    make -D actually work
    add 64bit support in date -d@num

--- src/date.c
+++ src/date.c
@@ -1,26 +1,27 @@
 #include "lib/common.h"
 
-#define parsedate(d) do {                                                         \
-                       char *_d = d;                                              \
-                       if (*_d == '@') tm = localtime(&(time_t){ atoi(_d+1) });   \
-                       else {                                                     \
-                         switch (strlen(_d)) {                                    \
-                           case  8: informat = "%m%d%H%M"    ; break;             \
-                           case 10: informat = "%m%d%H%M%y"  ; break;             \
-                           case 12: informat = "%m%d%H%M%C%y"; break;             \
-                           default: return -1;                                    \
-                         }                                                        \
-                         strptime(_d, flag('D') ? lastarg('D') : informat, tm);   \
-                         tm = localtime(&(time_t) { mktime(tm) });                \
-                       }                                                          \
-                     } while (0)
+static void parsedate(const char *d, char **informat, struct tm **tm) {
+  if (*d == '@') *tm = localtime(&(time_t){ strtol(d+1, 0, 10) });
+  else {
+    if (flag('D')) *informat = lastarg('D');
+    else
+      switch (strlen(d)) {
+        case  8: *informat = "%m%d%H%M"    ; break;
+        case 10: *informat = "%m%d%H%M%y"  ; break;
+        case 12: *informat = "%m%d%H%M%C%y"; break;
+        default: exit(-1);
+      }
+    strptime(d, *informat, *tm);
+    *tm = localtime(&(time_t) { mktime(*tm) });
+  }
+}
 
 int main(int argc, char *argv[]) {
   options("RuD:d:r:", .arglessthan = 2); // D like toybox and busybox
   char *informat, *outformat = flag('R') ? "%a, %d %b %Y %T %z" : "%a %b %e %H:%M:%S %Z %Y", outbuf[4096];
   if (flag('u')) setenv("TZ", "UTC", 1);
   struct tm *tm = localtime(&(time_t) { time(NULL) });
-  if (flag('d')) parsedate(lastarg('d'));
+  if (flag('d')) parsedate(lastarg('d'), &informat, &tm);
   else if (flag('r')) {
     struct stat st;
     if (stat(lastarg('r'), &st) == -1) return errno;
@@ -33,7 +34,7 @@ dodate:
     strftime(outbuf, sizeof(outbuf), outformat, tm);
   }
   else {
-    parsedate(argv[1]);
+    parsedate(argv[1], &informat, &tm);
     clock_settime(CLOCK_REALTIME, &(struct timespec) { mktime(tm), 0 });
     strftime(outbuf, sizeof(outbuf), outformat, localtime(&(time_t) { time(NULL) }));
   }
