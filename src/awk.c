76e70b9fbe47bd816b6c5f73d186fbf0d91cbe93
izabera <izaberina@gmail.com>
2020-05-03 22:03:23 +0100

    move between machines

--- src/awk.c
+++ src/awk.c
@@ -12,16 +12,23 @@ static htable *h;
 #define hdel(...) hdel(h, __VA_ARGS__)
 
 int main(int argc, char *argv[]) {
-  options("f:F:v:");
+  options("f:F:v:", .argleast = 1);
 
   struct str script;
   if (flag('f')) {
     FILE *memstream = open_memstream(&script.str, &script.len);
+    while (*++argv) {
+         if (argv[0][0] == '-' && argv[0][1] == 0) fileptr = stdin;
+    else if (!(fileptr = fopen(argv[0], "r"))) continue;
+inner:
+
     for (size_t i = 0; i < flag('f'); i++) {
-      FILE *f = fopen(flags[opt('f')].args[i], "r");
-      for (int c; (c = fgetc_unlocked(f)) != -1; )
-        fputc_unlocked(c, memstream);
-      fclose(f);
+      FILE *f;
+           if (!strcmp(flags[opt('f')].args[i], "-")) f = stdin;
+      else if (!(fileptr = fopen(flags[opt('f')].args[i], "r"))) continue;
+      for (int c; (c = fgetc(f)) != -1; )
+        fputc(c, memstream);
+      if (f != stdin) fclose(f);
     }
     fclose(memstream);
   }
@@ -39,29 +46,5 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  /*
-
-  size_t linecount = 1;
-  FILE *fileptr = stdin;
-  if (argc == 1)
-    goto inner;
-
-  while (*++argv) {
-    if (argv[0][0] == '-' && argv[0][1] == 0) { file = 0; fileptr = stdin; }
-    else if ((file = open(argv[0], O_RDONLY)) == -1) continue;
-    else fileptr = fdopen(file, "r");
-inner:
-
-    if (argc == 1)
-          goto inner;
-
-      while (*++argv) {
-
-  else if (argc > 1)
-
-
-  
-
-  */
   return 0;
 }
