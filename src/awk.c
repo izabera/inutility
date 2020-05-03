9a4308128836cf027c88b35507e830b09af46e70
izabera <izaberina@gmail.com>
2020-05-03 22:26:09 +0100

    remove obsolete comments and fix up fileptr

--- src/awk.c
+++ src/awk.c
@@ -1,11 +1,5 @@
 #include "lib/common.h"
 
-/*
-#define flag(x)    (flags[opt(x)].count)
-#define lastarg(x) (flags[opt(x)].args[flag(x)-1])
-#define lastnum(x) (flags[opt(x)].nums[flag(x)-1])
-*/
-
 static htable *h;
 #define hset(...) hset(h, __VA_ARGS__)
 #define hget(...) hget(h, __VA_ARGS__)
@@ -17,15 +11,10 @@ int main(int argc, char *argv[]) {
   struct str script;
   if (flag('f')) {
     FILE *memstream = open_memstream(&script.str, &script.len);
-    while (*++argv) {
-         if (argv[0][0] == '-' && argv[0][1] == 0) fileptr = stdin;
-    else if (!(fileptr = fopen(argv[0], "r"))) continue;
-inner:
-
     for (size_t i = 0; i < flag('f'); i++) {
       FILE *f;
            if (!strcmp(flags[opt('f')].args[i], "-")) f = stdin;
-      else if (!(fileptr = fopen(flags[opt('f')].args[i], "r"))) continue;
+      else if (!(f = fopen(flags[opt('f')].args[i], "r"))) continue;
       for (int c; (c = fgetc(f)) != -1; )
         fputc(c, memstream);
       if (f != stdin) fclose(f);
